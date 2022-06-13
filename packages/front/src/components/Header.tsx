a1bd7b6bfdc8c61e8e755c51c271da3a5cd74a82
fenfil <kennyvova@ya.ru>
2022-06-14 01:28:05 +0300

    s

--- packages/front/src/components/Header.tsx
+++ packages/front/src/components/Header.tsx
@@ -6,9 +6,26 @@ export const Header = () => {
   const user = useTypedSelector((s) => s.user.name);
 
   return (
-    <div>
-      <span>Hi {user || 'guest'}</span>
-      <Navigation />
-    </div>
+    <header className="header">
+      <div className="container">
+        <div className="header__inner">
+          <div className="header__logo">four wheels</div>
+          <nav className="nav">
+            <a className="nav__link" href="#">
+              About
+            </a>
+            <a className="nav__link" href="#">
+              Service
+            </a>
+            <a className="nav__link" href="#">
+              Work
+            </a>
+            <a className="nav__link" href="#">
+              Verified
+            </a>
+          </nav>
+        </div>
+      </div>
+    </header>
   );
 };
